3c3a45760d40b2211b3786ab928b511c524d56fd
syshawn <lsy2pw@gmail.com>
2022-02-11 02:05:50 +0800

    ENTER

--- main.js
+++ main.js
@@ -17,32 +17,47 @@ const navBar = document.getElementById('nav-bar'),
         })
     }
 
-    /* Remove */
-    const navLink = document.querySelectorAll('.nav_link')
+/* Remove */
+const navLink = document.querySelectorAll('.nav_link')
 
-        function linkAction(){
-            const navBar = document.getElementById('nav-bar')
-            navBar.classList.remove('show-list')
-        }
-        navLink.forEach(n => n.addEventListener('click', linkAction))
+    function linkAction(){
+        const navBar = document.getElementById('nav-bar')
+        navBar.classList.remove('show-list')
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-    /* Qualification Switch-Tab Function */
-    const tabs = document.querySelectorAll('[data-target]'),
-        tabContents = document.querySelectorAll('[data-content]')
+/* Qualification Switch-Tab Function */
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
         
-        tabs.forEach(tab =>{
-            tab.addEventListener('click', () =>{
-                const target = document.querySelector(tab.dataset.target)
+    tabs.forEach(tab =>{
+        tab.addEventListener('click', () =>{
+            const target = document.querySelector(tab.dataset.target)
 
-                tabContents.forEach(tabContent =>{
-                    tabContent.classList.remove('qualification_active')
-                    })
-                    target.classList.add('qualification_active')
-
-                    tabs.forEach(tab =>{
-                        tab.classList.remove('qualification_active')
-                    })
-                    tab.classList.add('qualification_active')
+            tabContents.forEach(tabContent =>{
+                tabContent.classList.remove('qualification_active')
                 })
+                target.classList.add('qualification_active')
+
+                tabs.forEach(tab =>{
+                    tab.classList.remove('qualification_active')
+                })
+                tab.classList.add('qualification_active')
             })
+        })
+
+/* Sweeper Function */
+let swiper = new swiper(".portfolio_container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
