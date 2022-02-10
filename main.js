57d8f74079b7c3bbb15ce8be5f24b6abe33495f2
syshawn <lsy2pw@gmail.com>
2022-02-11 05:00:42 +0800

    en ter

--- main.js
+++ main.js
@@ -61,3 +61,37 @@ let swiper = new Swiper(".portfolio_container", {
       clickable: true,
     },
   });
+
+/* Scoll Section Active Link*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId =  current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('nav-bar a[href*=' + sectionId +']').classList.add('active-link')
+        } else {
+            document.querySelector('nav-bar a[href*=' + sectionId +']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* Change Background Header */
+function scrollHeader(){
+    const nav = document.getElementById('header')
+        if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* Show Scroll to Top */
+function scrollTop() {
+    const scrollTop = document.getElementById('scrolltotop');
+        if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
