665c2f06646991c252d0426662f4b6a954de4ddf
syshawn <lsy2pw@gmail.com>
2022-02-10 17:13:32 +0800

    enter

--- main.js
+++ main.js
@@ -25,3 +25,24 @@ const navBar = document.getElementById('nav-bar'),
             navBar.classList.remove('show-list')
         }
         navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+    /* Qualification Switch-Tab Function */
+    const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
+        
+        tabs.forEach(tab =>{
+            tab.addEventListener('click', () =>{
+                const target = document.querySelector(tab.dataset.target)
+
+                tabContents.forEach(tabContent =>{
+                    tabContent.classList.remove('qualification_active')
+                    })
+                    target.classList.add('qualification_active')
+
+                    tabs.forEach(tab =>{
+                        tab.classList.remove('qualification_active')
+                    })
+                    tab.classList.add('qualification_active')
+                })
+            })
